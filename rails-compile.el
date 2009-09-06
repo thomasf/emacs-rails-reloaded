25024089e48ac72f1d1fccfcbcd8e14e1ff65c90
Aleksei Gusev <gusev@tux.(none)>
2009-09-06 16:31:24 +0300

    added 'warning' status for vendor/ files in compilation output.

--- rails-compile.el
+++ rails-compile.el
@@ -62,8 +62,8 @@
 (defun rails/compile/error-regexp-alist ()
   (list
    (list 'rails/compile/error
-				 "\\(?:\\[\\|^\\|\\s+\\|(\\)?\\([^ :\n\]+\\):\\([0-9]+\\)+\\b"
-				 1 2 nil 2 1)))
+				 "\\(?:\\[\\|^\\|\\s+\\|(\\)?\\(\\([^ :\n\]+vendor/[^ :\n\]+\\)\\|[^ :\n\]+\\):\\([0-9]+\\)+\\b"
+				 1 3 nil '(2) 1)))
 
 
 (define-derived-mode rails/compilation-mode compilation-mode "RCompile"
